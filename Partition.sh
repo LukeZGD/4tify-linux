480363e314911a92d9a727806df63ad6bc4c6ae9
alesx2io <a.guarnieri2005@icloud.com>
2022-08-22 17:59:03 +0200

    fixed Partition.sh to work on linux

--- Partition.sh
+++ Partition.sh
@@ -2,26 +2,25 @@
 set -e
 IP=$1
 cd support_files/4.3/File_System
-/usr/bin/expect <(cat << EOF
-set timeout -1
-spawn ssh -o StrictHostKeyChecking=no -p 22 root@$IP
-expect "root@$IP's password:"
-send "alpine\r"
-expect "#"
-send "TwistedMind2 -d1 3221225472 -s2 879124480 -d2 max\r"
-expect "#"
-send "exit\r"
-expect eof
+
+iproxy 2222 22 & /usr/bin/expect <(cat << 'EOD'
+    set timeout -1
+    spawn ssh -o StrictHostKeyChecking=no -p 2222 root@localhost
+    expect "root@localhost's password:"
+    send "alpine\r"
+    expect "#"
+    send "TwistedMind2 -d1 3221225472 -s2 879124480 -d2 max\r"
+    expect "#"
+    send "exit\r"
+    expect eof
+EOD
 )
 sleep 2
 echo "Fetching Patch File"
-srcdirs=$(ssh -n -p 22 root@$IP "find / -name '*TwistedMind2-*'")
+
+srcdirs=$(ssh -n -p 2222 root@localhost "find / -name '*TwistedMind2-*'")
 echo "$srcdirs"
-/usr/bin/expect <(cat << EOF
-set timeout -1
-spawn scp -P 22 -o StrictHostKeyChecking=no root@$IP:$srcdirs $(pwd)
-expect "root@$IP's password:"
-send "alpine\r"
-expect eof
-)
+
+scp -P 2222 -o StrictHostKeyChecking=no root@localhost:$srcdirs $(pwd)
+    
 echo "Done!"
